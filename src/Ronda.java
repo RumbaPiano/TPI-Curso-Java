6f6c68be5d74ea782d26ee57e01216ecd4b53867
Tomas Pubill <tomaspubill@gmail.com>
2023-03-18 15:37:42 -0300

    PRIMER COMMIT

---
+++ src/Ronda.java
@@ -0,0 +1,12 @@
+public class Ronda {
+    private String nro;
+    private Partido[] partidos;
+
+    public Ronda(String nro, Partido[] partidos){
+        this.nro=nro;
+        this.partidos=partidos;
+    }
+
+    public int puntos(){
+    }
+}
